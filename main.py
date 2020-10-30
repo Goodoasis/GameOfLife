2e1360e6c9289a5b958e9f69ce1b5e2633fc0866
GoodOasis <de.thibault@gmail.com>
2020-10-30 22:59:03 +0100

    Start project

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class Cellife():
+    def __init__(self):
+        self.create_widget()
+        pass
+
+    def create_widget(self):
+        pass
+
+if __name == "__main__":
+    APP = cellife())
+    APP.window.mainloop()
