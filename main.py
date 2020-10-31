48812f010d8a394fe0f7bddb935585e75e378342
GoodOasis <de.thibault@gmail.com>
2020-11-01 00:34:19 +0100

    Clean and comment code

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 from tkinter import *
 
 class Cellife():
+    """[summary]
+    """
     def __init__(self):
+        """[summary]
+        """
         self.window = Tk()
         self.window.title = "Cel*life"
         self.window.geometry("1100x950")
         self.window.config(background='brown')
-        # Init frames
+        # Init frames.
         self.frame = Frame(self.window, bg='red')
         self.frame_sim = Frame(self.frame, bg='green', border=1)
         self.frame_time = Frame(self.frame_sim, bg="yellow", border=1)
-
-        # Create all of widgets
+        # Create all of widgets.
         self.create_widgets()
-        # Class Variables
+        # Class Variables.
         self.res = self.res_slider.get()
-
-        # pack
+        # Pack.
         self.frame.pack(expand=YES)
         self.frame_sim.grid(pady=40, row=0, column=1, sticky=N)
         self.frame_time.pack(expand=YES)
 
     def create_widgets(self):
-        # Labels
+        # Labels.
         self.create_title()
-        # Buttons
+        # Buttons.
         self.create_start_button()
         self.create_next_button()
         self.create_prev_button()
         self.create_quit_button()
-        # Slider
+        # Slider.
         self.create_slider()
-        # Canvas
+        # Canvas.
         self.create_canvas()
     
     def create_title(self):
@@ -55,7 +57,7 @@ class Cellife():
         self.quit_button.pack(side=BOTTOM)
 
     def create_slider(self):
-        self.res_slider = Scale(self.frame_sim ,label="Resolution", orient=HORIZONTAL, from_=100, to=5, resolution=5, command=self._draw_grid)
+        self.res_slider = Scale(self.frame_sim ,label="Resolution", orient=HORIZONTAL, from_=70, to=5, resolution=5, command=self._draw_grid)
         self.res_slider.set(25)
         self.res_slider.pack()
 
@@ -66,13 +68,14 @@ class Cellife():
     def _draw_grid(self, slider_value):
         width = int(self.canvas['width'])
         height = int(self.canvas['height'])
-
-        step_x = 0
-        step_y = 0
+        # Clear canvas
         self.canvas.delete('all')
+        # Draw lines horizontals then verticals.
+        step_x = 0
         while step_x < width:
             self.canvas.create_line(step_x, 0, step_x, height, width=1, fill='black')
             step_x += int(slider_value)
+        step_y = 0
         while step_y < height:
             self.canvas.create_line(0, step_y, width, step_y, width=1, fill='black')
             step_y += int(slider_value)
