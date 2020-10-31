636b279fff03af393c712ea5fcc51776a71558f7
GoodOasis <de.thibault@gmail.com>
2020-11-01 00:28:39 +0100

    Implement slider for grid resolution

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ class Cellife():
         self.window.title = "Cel*life"
         self.window.geometry("1100x950")
         self.window.config(background='brown')
-
+        # Init frames
         self.frame = Frame(self.window, bg='red')
         self.frame_sim = Frame(self.frame, bg='green', border=1)
-        self.frame_time = Frame(self.frame_sim, bg="yellow", border=2)
+        self.frame_time = Frame(self.frame_sim, bg="yellow", border=1)
 
         # Create all of widgets
         self.create_widgets()
+        # Class Variables
+        self.res = self.res_slider.get()
 
         # pack
         self.frame.pack(expand=YES)
@@ -20,13 +22,15 @@ class Cellife():
         self.frame_time.pack(expand=YES)
 
     def create_widgets(self):
+        # Labels
+        self.create_title()
         # Buttons
         self.create_start_button()
         self.create_next_button()
         self.create_prev_button()
         self.create_quit_button()
-        # Labels
-        self.create_title()
+        # Slider
+        self.create_slider()
         # Canvas
         self.create_canvas()
     
@@ -34,10 +38,6 @@ class Cellife():
         self.title = Label(self.frame, text="Welcome to cell life")
         self.title.grid(row=0, column=1, sticky=N)
 
-    def create_canvas(self):
-        self.canvas = Canvas(self.frame, bg='gray', width=900, height=900)
-        self.canvas.grid(row=0, column=0)
-    
     def create_start_button(self):
         self.start_button = Button(self.frame_sim, text="Start/Stop")
         self.start_button.pack()
@@ -53,6 +53,29 @@ class Cellife():
     def create_quit_button(self):
         self.quit_button = Button(self.frame_sim, text="Quit", command=self.window.quit)
         self.quit_button.pack(side=BOTTOM)
+
+    def create_slider(self):
+        self.res_slider = Scale(self.frame_sim ,label="Resolution", orient=HORIZONTAL, from_=100, to=5, resolution=5, command=self._draw_grid)
+        self.res_slider.set(25)
+        self.res_slider.pack()
+
+    def create_canvas(self):
+        self.canvas = Canvas(self.frame, bg='white', width=900, height=900)
+        self.canvas.grid(row=0, column=0)
+
+    def _draw_grid(self, slider_value):
+        width = int(self.canvas['width'])
+        height = int(self.canvas['height'])
+
+        step_x = 0
+        step_y = 0
+        self.canvas.delete('all')
+        while step_x < width:
+            self.canvas.create_line(step_x, 0, step_x, height, width=1, fill='black')
+            step_x += int(slider_value)
+        while step_y < height:
+            self.canvas.create_line(0, step_y, width, step_y, width=1, fill='black')
+            step_y += int(slider_value)
 
 if __name__ == "__main__":
     APP = Cellife()
